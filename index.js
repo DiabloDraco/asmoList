4d212944707d48e3c339c7671c086a3baa44db66
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-09-19 15:15:42 +0500

    lol

---
+++ index.js
@@ -0,0 +1,19 @@
+let arr = ['Bahrom Ilamanovich' , 'Murod Abdujabborovich' , 'Javohir Sharobidinovich' , 'Barno Yuldashevna' , 'Grigoriy Borisovich' , 'Malika Axmadjonovna' , 'Suhrob Xamrakulov' , 'Evelina Koldarova' , 'Xalitov Imil Ilnurovich' , 'Shahrizoda Bahodirovna']
+
+let num = [0 , 1 , 2, 3 , 4 ,5 ,6 ,7 , 8 , 9]
+
+
+let elList = document.querySelector(".list__wrapper")
+
+let t = arr.sort(()=>Math.random()-0.5)
+
+
+function render() {
+    for (let i = 0; i < t.length; i++) {
+        let div = document.createElement("div")
+        div.textContent = t[i]
+        elList.appendChild(div)
+    }
+}
+
+render()
