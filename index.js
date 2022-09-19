8a67882bc19ea33e6f860ebd82e688af097948fa
DiabloDraco <mirmuhsinmirahmatov@gmail.com>
2022-09-19 15:23:13 +0500

    dadada

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let arr = ['Bahrom Ilamanovich' , 'Murod Abdujabborovich' , 'Javohir Sharobidinovich' , 'Barno Yuldashevna' , 'Grigoriy Borisovich' , 'Malika Axmadjonovna' , 'Suhrob Xamrakulov' , 'Evelina Koldarova' , 'Xalitov Imil Ilnurovich' , 'Shahrizoda Bahodirovna']
 
-let num = [0 , 1 , 2, 3 , 4 ,5 ,6 ,7 , 8 , 9]
+// Just for test
+// let num = [0 , 1 , 2, 3 , 4 ,5 ,6 ,7 , 8 , 9]
 
 
 let elList = document.querySelector(".list__wrapper")
